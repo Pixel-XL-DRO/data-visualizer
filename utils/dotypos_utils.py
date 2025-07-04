7c874801d129fc68552554441d3eb588e0ff8ec9
Maciej Burzawa <maciej.burzawa@pixel-xl.com>
2025-07-04 10:53:40 +0200

    fix

--- utils/dotypos_utils.py
+++ utils/dotypos_utils.py
@@ -26,10 +26,10 @@ def group_order_items(df, group_by, moving_average_days):
         df_grouped = df_grouped.sort_values(['date_only', group_by])
 
         df_grouped['netto_rolling_avg'] = df_grouped.groupby(group_by)['netto'].transform(
-            lambda x: x.rolling(window=moving_average_days, min_periods=1).mean())
+            lambda x: x.rolling(window=moving_average_days, min_periods=moving_average_days).mean())
 
         df_grouped['brutto_rolling_avg'] = df_grouped.groupby(group_by)['brutto'].transform(
-            lambda x: x.rolling(window=moving_average_days, min_periods=1).mean())
+            lambda x: x.rolling(window=moving_average_days, min_periods=moving_average_days).mean())
 
         df_upsell_grouped = df_upsell.groupby(['date_only', df_upsell[group_by]]).agg(
             quantity=('quantity', 'sum'),
@@ -40,13 +40,13 @@ def group_order_items(df, group_by, moving_average_days):
         df_upsell_grouped = df_upsell_grouped.sort_values(['date_only', group_by])
 
         df_upsell_grouped['avg_netto_per_purchase'] = df_upsell_grouped.groupby(group_by).apply(
-            lambda g: (g['netto'].rolling(window=moving_average_days, min_periods=1).sum() /
-                       g['quantity'].rolling(window=moving_average_days, min_periods=1).sum())
+            lambda g: (g['netto'].rolling(window=moving_average_days, min_periods=moving_average_days).sum() /
+                       g['quantity'].rolling(window=moving_average_days, min_periods=moving_average_days).sum())
         ).reset_index(level=0, drop=True)
 
         df_upsell_grouped['avg_brutto_per_purchase'] = df_upsell_grouped.groupby(group_by).apply(
-            lambda g: (g['brutto'].rolling(window=moving_average_days, min_periods=1).sum() /
-                       g['quantity'].rolling(window=moving_average_days, min_periods=1).sum())
+            lambda g: (g['brutto'].rolling(window=moving_average_days, min_periods=moving_average_days).sum() /
+                       g['quantity'].rolling(window=moving_average_days, min_periods=moving_average_days).sum())
         ).reset_index(level=0, drop=True)
 
     else:
@@ -59,10 +59,10 @@ def group_order_items(df, group_by, moving_average_days):
         df_grouped = df_grouped.sort_values('date_only')
 
         df_grouped['netto_rolling_avg'] = df_grouped['netto'].rolling(
-            window=moving_average_days, min_periods=1).mean()
+            window=moving_average_days, min_periods=moving_average_days).mean()
 
         df_grouped['brutto_rolling_avg'] = df_grouped['brutto'].rolling(
-            window=moving_average_days, min_periods=1).mean()
+            window=moving_average_days, min_periods=moving_average_days).mean()
 
         df_upsell_grouped = df_upsell.groupby('date_only').agg(
             quantity=('quantity', 'sum'),
@@ -72,13 +72,13 @@ def group_order_items(df, group_by, moving_average_days):
 
         # df_upsell_grouped = df_upsell_grouped.sort_values('date_only') #czasami to cos nie dziala ale narazie dziala
         df_upsell_grouped['avg_netto_per_purchase'] = (
-            df_upsell_grouped['netto'].rolling(window=moving_average_days, min_periods=1).sum() /
-            df_upsell_grouped['quantity'].rolling(window=moving_average_days, min_periods=1).sum()
+            df_upsell_grouped['netto'].rolling(window=moving_average_days, min_periods=moving_average_days).sum() /
+            df_upsell_grouped['quantity'].rolling(window=moving_average_days, min_periods=moving_average_days).sum()
         )
 
         df_upsell_grouped['avg_brutto_per_purchase'] = (
-            df_upsell_grouped['brutto'].rolling(window=moving_average_days, min_periods=1).sum() /
-            df_upsell_grouped['quantity'].rolling(window=moving_average_days, min_periods=1).sum()
+            df_upsell_grouped['brutto'].rolling(window=moving_average_days, min_periods=moving_average_days).sum() /
+            df_upsell_grouped['quantity'].rolling(window=moving_average_days, min_periods=moving_average_days).sum()
         )
 
     df_upsell_grouped['brutto_by_quantity'] = df_upsell_grouped['brutto'] / df_upsell_grouped['quantity']
@@ -87,6 +87,9 @@ def group_order_items(df, group_by, moving_average_days):
     return df_grouped, df_upsell_grouped
 
 def calc_earnings_per_reservation(df_reservation, df, group_by, moving_average_days):
+
+    df = df[df['status'] == 'closed']
+
     df['brutto'] = pd.to_numeric(df['brutto'], errors='coerce')
     df['netto'] = pd.to_numeric(df['netto'], errors='coerce')
 
@@ -112,18 +115,18 @@ def calc_earnings_per_reservation(df_reservation, df, group_by, moving_average_d
         df_result['mean_netto_per_reservation'] = df_result['total_netto'] / df_result['count']
 
         df_result['count_rolling_avg'] = df_result.groupby(group_by)['count'].transform(
-            lambda x: x.rolling(window=moving_average_days, min_periods=1).mean())
+            lambda x: x.rolling(window=moving_average_days, min_periods=moving_average_days).mean())
 
         df_result['mean_brutto_per_reservation_rolling_avg'] = df_result.groupby(group_by)['mean_brutto_per_reservation'].transform(
-            lambda x: x.rolling(window=moving_average_days, min_periods=1).mean())
+            lambda x: x.rolling(window=moving_average_days, min_periods=moving_average_days).mean())
 
         df_result['mean_netto_per_reservation_rolling_avg'] = df_result.groupby(group_by)['mean_netto_per_reservation'].transform(
-            lambda x: x.rolling(window=moving_average_days, min_periods=1).mean())
+            lambda x: x.rolling(window=moving_average_days, min_periods=moving_average_days).mean())
 
         df_result['total_brutto_income'] = df_result['total_brutto'] + df_result['total_reservations_value']
 
         df_result['total_brutto_income_rolling_avg'] = df_result.groupby(group_by)['total_brutto_income'].transform(
-            lambda x: x.rolling(window=moving_average_days, min_periods=1).mean())
+            lambda x: x.rolling(window=moving_average_days, min_periods=moving_average_days).mean())
 
     else:
         df_grouped = df_reservation.groupby('start_date').agg(
@@ -144,16 +147,16 @@ def calc_earnings_per_reservation(df_reservation, df, group_by, moving_average_d
         df_result['mean_netto_per_reservation'] = df_result['total_netto'] / df_result['count']
 
         df_result['count_rolling_avg'] = df_result['count'].rolling(
-            window=moving_average_days, min_periods=1).mean()
+            window=moving_average_days, min_periods=moving_average_days).mean()
 
         df_result['mean_brutto_per_reservation_rolling_avg'] = df_result['mean_brutto_per_reservation'].rolling(
-            window=moving_average_days, min_periods=1).mean()
+            window=moving_average_days, min_periods=moving_average_days).mean()
 
         df_result['mean_netto_per_reservation_rolling_avg'] = df_result['mean_netto_per_reservation'].rolling(
-            window=moving_average_days, min_periods=1).mean()
+            window=moving_average_days, min_periods=moving_average_days).mean()
 
         df_result['total_brutto_income'] = df_result['total_brutto'] + df_result['total_reservations_value']
         df_result['total_brutto_income_rolling_avg'] = df_result['total_brutto_income'].rolling(
-            window=moving_average_days, min_periods=1).mean()
+            window=moving_average_days, min_periods=moving_average_days).mean()
 
     return df_result
