295bd92bd0c2bfcfc90d6b12780960c58e1d6da7
Maciej Burzawa <maciej.burzawa@pixel-xl.com>
2025-09-03 08:55:35 +0200

    display retention months in better way

--- utils/clients_utils.py
+++ utils/clients_utils.py
@@ -1,3 +1,5 @@
+import utils
+
 def group_data(df, x_axis_type, grouping_field):
   if grouping_field:
     df_grouped = df.groupby([df[x_axis_type].dt.to_period('M'), df[grouping_field]]).agg(
@@ -12,7 +14,7 @@ def group_data(df, x_axis_type, grouping_field):
       past_reservation_clients=('has_past_reservation', 'sum')
     ).reset_index()
 
-  df_grouped[x_axis_type] = df_grouped[x_axis_type].dt.to_timestamp()
+  df_grouped[x_axis_type] = df_grouped[x_axis_type].dt.month.map(utils.get_month_from_month_number)
   df_grouped['retention_percent'] = df_grouped['returning_clients'] / df_grouped['reservations'] * 100
   df_grouped['past_retention_percent'] = df_grouped['past_reservation_clients'] / df_grouped['reservations'] * 100
 
