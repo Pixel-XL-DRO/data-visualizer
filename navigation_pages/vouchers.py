e8ca4e9a8d5b3b14205cfe8b82d74802c746b338
Maciej Burzawa <maciej.burzawa@pixel-xl.com>
2026-01-14 09:51:09 +0100

    adjust vouchers

--- navigation_pages/vouchers.py
+++ navigation_pages/vouchers.py
@@ -18,10 +18,18 @@ with st.spinner("Inicjalizacja...", show_time=True):
 (start_date, end_date, cities) = vouchers_sidebar.filter_data(df)
 
 with st.spinner("Ładowanie danych...", show_time=True):
-    df_vouchers = vouchers_queries.get_vouchers_count(start_date, end_date, cities)
 
-st.text("Ilość sprzedanych voucherów według nazwy")
-voucher_count_chart = utils.create_bar_chart(df_vouchers, 'name', 'Nazwa', 'count', 'Liczba sprzedanych kopii', None)
-st.altair_chart(voucher_count_chart, use_container_width=True)
+    df_vouchers, df_vouchers_worth = utils.run_in_parallel(
+        (vouchers_queries.get_vouchers_count, (start_date, end_date, cities)),
+        (vouchers_queries.get_vouchers_worth, (start_date, end_date, cities)),
+    )
+
+    st.text("Ilość sprzedanych voucherów według nazwy")
+    voucher_count_chart = utils.create_bar_chart(df_vouchers, 'name', 'Nazwa', 'count', 'Liczba sprzedanych kopii', None)
+    st.altair_chart(voucher_count_chart, use_container_width=True)
+
+    st.text("Wartość sprzedanych voucherów według nazwy")
+    voucher_worth_chart = utils.create_bar_chart(df_vouchers_worth, 'name', 'Nazwa', 'worth', 'Wartość sprzedanych kopii', None)
+    st.altair_chart(voucher_worth_chart, use_container_width=True)
 
 utils.lazy_load_initials()
