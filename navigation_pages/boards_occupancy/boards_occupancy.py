ca5feb7dedfb6902635125430ed4d9187429357e
Maciej Olejnik <maciej.olejnik@pixel-xl.com>
2026-01-22 10:29:52 +0100

    added warszawa box to safi view for boards occupancy

--- navigation_pages/boards_occupancy/boards_occupancy.py
+++ navigation_pages/boards_occupancy/boards_occupancy.py
@@ -14,7 +14,7 @@ import auth
 import plan4u_view
 import safi_view
 
-SAFI_CITIES = ["lubicz", "ogrodowa", "kijowska", "swietego-marcina", "sokolska", "grunwaldzka"]
+SAFI_CITIES = ["lubicz", "ogrodowa", "kijowska", "swietego-marcina", "sokolska", "grunwaldzka", "arkadia"]
 
 with st.spinner("Ładowanie danych...", show_time=True):
 
