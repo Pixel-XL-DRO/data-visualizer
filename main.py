2fd4d9975f34af8e7ea40dfd78211bf9cde52b75
Maciej Burzawa <maciej.burzawa@pixel-xl.com>
2026-02-11 15:04:35 +0100

    add voucher report to marketing, admin

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ pages_by_role = {
   },
   "admin": {
     "Rezerwacje": [reservations_page, clients_page, boards_occupancy_page, reservations_by_time_period_page, reservations_cumulative_page, dotypos, income, vouchers],
+    "Raporty": [voucher_report_page],
     "Opinie": [google_reviews_page, reviews_page],
     "Admin Panel": [data_editor_page],
     "Konto": [logout_page]
@@ -51,6 +52,7 @@ pages_by_role = {
   },
   "marketing": {
     "Rezerwacje": [reservations_page, clients_page, boards_occupancy_page, reservations_by_time_period_page, vouchers],
+    "Raporty": [voucher_report_page],
     "Opinie": [google_reviews_page, reviews_page],
     "Konto": [logout_page]
   }
