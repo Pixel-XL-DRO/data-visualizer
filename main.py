80311f002ecd19c656667d456e7f082d676de2ba
Maciej Burzawa <maciej.burzawa@pixel-xl.com>
2026-04-30 08:52:19 +0200

    add not started reservations report

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ financial_report_page = st.Page("navigation_pages/reports/financial_report.py", 
 safi_products_page = st.Page("navigation_pages/safi_products.py", title="Produkty dodatkowe online", icon=":material/add_shopping_cart:")
 voucher_report_page = st.Page("navigation_pages/reports/voucher_report.py", title = "Raport kodów promocyjnych", icon=":material/sell:")
 reservations_for_year_page = st.Page("navigation_pages/reports/visits_report.py", title = "Rezerwacje z fakturą", icon=":material/vertical_split:")
-clients_report = st.Page("navigation_pages/reports/clients_report.py", title = "Klienci", icon=":material/vertical_split:")
+clients_report = st.Page("navigation_pages/reports/clients_report.py", title = "Klienci", icon=":material/groups_3:")
+not_started_reservations_report = st.Page("navigation_pages/reports/not_started_reservations_report.py", title = "Raport rezerwacji nieodbytych", icon=":material/event_busy:")
 
 # safi_products_page is hidden until we differentiate products bought and added in data parser
 
@@ -39,7 +40,7 @@ pages_by_role = {
   "super-admin": {
     "Rezerwacje": [reservations_page, clients_page, boards_occupancy_page, reservations_by_time_period_page, reservations_cumulative_page],
     "Sprzedaż": [dotypos, income, vouchers],
-    "Raporty": [financial_report_page, reservations_for_year_page, voucher_report_page, clients_report],
+    "Raporty": [financial_report_page, reservations_for_year_page, voucher_report_page, clients_report, not_started_reservations_report],
     "Opinie": [google_reviews_page, reviews_page],
     "Admin Panel": [data_editor_page],
     "Konto": [logout_page]
---
+++ shared/queries/report_queries/not_started_reservation_queries.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from queries import run_query
+import utils
+
+def get_started_reservation_percent_without_mark_as_started(reservations_ids):
+
+  reservations_ids_to_use = utils.format_array_for_query(reservations_ids)
+
+  query = f"""
+    SELECT 
+      COUNT(DISTINCT ess.reservation_external_id) AS started_but_unchecked
+    FROM
+      board_playthroughs.event_start_session ess
+    WHERE 
+      ess.reservation_external_id {reservations_ids_to_use}
+  """
+
+  rows = run_query(query)
+  return pd.DataFrame(rows)
+
+
+
+
+
